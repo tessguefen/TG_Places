9e5deb76e5173103d7523430e422c45b1997b923
Tess <tguefen@gmail.com>
2017-04-21 22:51:48 -0700

    Yaaaaaaaaaaaaaas
    Stuff works now

--- js/TGPlaces_MapPopup.js
+++ js/TGPlaces_MapPopup.js
@@ -1,5 +1,4 @@
-function TGPlaces_MapPopup_Column( header_text, code, fieldname )
-{
+function TGPlaces_MapPopup_Column( header_text, code, fieldname ) {
 	MMBatchList_Column.call( this, header_text, code );
 
 	this.SetSortByField( code );
@@ -39,7 +38,135 @@ TGPlaces_MapPopup_Column.prototype.onDisplayEdit = function( record, item ) {
 	return container;
 }
 
-TGPlaces_MapPopup_Column.prototype.Lookup = function( item )
-{
-	console.log('test');
+TGPlaces_MapPopup_Column.prototype.Lookup = function( item ) {
+	googlemaps_dialog = new TGPlaces_GoogleMap_Popup();
+	googlemaps_dialog.AddPlace	= function() {
+		var i, i_len, inputlist;
+
+		if ( !item.row || !selected_place ) {
+			return;
+		}
+
+		console.log(selected_place, item);
+
+		inputlist = item.row[ 'column_placeId' ].getElementsByTagName( 'input' );
+
+		for ( i = 0, i_len = inputlist.length; i < i_len; i++ ){
+			if ( inputlist[ i ].name == 'placeId' ){
+				inputlist[ i ].value = selected_place;
+				this.Hide();
+				return;
+			}
+		}
+	};
+	googlemaps_dialog.Show();
+}
+
+
+/*
+ =====================
+ */
+function TGPlaces_GoogleMap_Popup(){
+	var self = this;
+	var selected_place = '';
+
+	MMDialog.call( this, 'tg_places_googlemap', 575, 575 );
+
+	this.SetResizeEnabled();
+	this.SetTitle( 'Search for a Thingy' );
+
+	// Controls
+	this.button_cancel				= this.ActionItem_Add( 'Cancel',					function() { self.Cancel(); } );
+	this.button_add_place			= this.ActionItem_Add( 'Add Selected Place',		function() { self.AddPlace(); } );
+
+	this.button_add_place.Hide();
+}
+
+DeriveFrom( MMDialog, TGPlaces_GoogleMap_Popup );
+var run = 1;
+
+TGPlaces_GoogleMap_Popup.prototype.onModalShow = function( z_index ){
+	MMDialog.prototype.onModalShow.call( this, z_index );
+	initMap(run);
+	run = 0;
+}
+
+TGPlaces_GoogleMap_Popup.prototype.onSetContent = function() {
+	this.button_add_place.Show();
+}
+
+TGPlaces_GoogleMap_Popup.prototype.Cancel = function(){
+	this.Hide();
+	this.oncancel();
+}
+
+TGPlaces_GoogleMap_Popup.prototype.AddPlace = function() {
+	console.log(selected_place);
+	this.Hide();
+}
+
+TGPlaces_GoogleMap_Popup.prototype.oncancel = function() { ; }
+
+
+function initMap( run ) {
+	if (!run) {
+		return;
+	}
+	selected_place = '';
+	var map = new google.maps.Map(document.getElementById('TGPlaces_map'), {
+		center: {lat: 39.50, lng: -98.35},
+		zoom: 4
+	});
+
+	var input = document.getElementById('pac-input');
+
+	var autocomplete = new google.maps.places.Autocomplete(input);
+	autocomplete.bindTo('bounds', map);
+
+	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+	var infowindow = new google.maps.InfoWindow();
+
+	var marker = new google.maps.Marker({
+		map: map
+	});
+
+	marker.addListener('click', function() {
+		var purty = place.formatted_address.match(/([^,]*),(.*)/);
+		infowindow.setContent('<div class="gm-info secondary-font"><strong class="gm-info--name">' + place.name + '</strong>' +
+			'<div class="gm-info--address">' +
+			purty[1] + '<br />' + purty[2]);
+		infowindow.open(map, marker);
+		selected_place = place.place_id;
+	});
+
+	autocomplete.addListener('place_changed', function() {
+		infowindow.close();
+		var place = autocomplete.getPlace();
+
+		if (!place.geometry) {
+			return;
+		}
+
+		if (place.geometry.viewport) {
+			map.fitBounds(place.geometry.viewport);
+		} else {
+			map.setCenter(place.geometry.location);
+			map.setZoom(17);
+		}
+
+		// Set the position of the marker using the place ID and location.
+		marker.setPlace({
+			placeId: place.place_id,
+			location: place.geometry.location
+		});
+		marker.setVisible(true);
+
+		var purty = place.formatted_address.match(/([^,]*),(.*)/);
+		infowindow.setContent('<div class="gm-info secondary-font"><strong class="gm-info--name">' + place.name + '</strong>' +
+			'<div class="gm-info--address">' +
+			purty[1] + '<br />' + purty[2]);
+		infowindow.open(map, marker);
+		selected_place = place.place_id;
+	});
 }
